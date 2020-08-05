043ffb8eef3a219c0c124180ca03b80ae473f9ec
GaliyaZ <galiya753@gmail.com>
2020-08-06 02:38:15 +0500

    add header desktop

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,13 @@
 let menuOpen = document.querySelector('.nav__toggle');
 let menuNav = document.querySelector('.nav__list');
+let windowWidth = document.documentElement.clientWidth;
 
 menuOpen.addEventListener('click', function(evt) {
   menuNav.classList.toggle('visually-hidden');
   menuOpen.classList.toggle('nav__toggle--closed');
   menuOpen.classList.toggle('nav__toggle--opened');
 });
+
+if (windowWidth >= 1200) {
+  menuNav.classList.toggle('visually-hidden');
+}
